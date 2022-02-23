67c1c92f02c751eff8063e3191b44b910166a0e2
redmanout <vladimirkotov1115@gmail.com>
2022-02-23 11:39:45 +0200

    created a model for sending messages to mail

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    sender_login = models.CharField(
+        max_length=50,
+        verbose_name='Логин отправителя'
+    )
+    email = models.EmailField(
+        max_length=100,
+        verbose_name='Email отправителя*'
+    )
+    topic_letter = models.CharField(
+        max_length=200,
+        verbose_name='Тема письма*'
+    )
+    text_message = models.TextField(
+        max_length=1500,
+        verbose_name='Текст сообщения*'
+    )
+
+    def __str__(self):
+        return f'Пользователь с логином {self.sender_login} отправил письмо на email:{self.email}'
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
